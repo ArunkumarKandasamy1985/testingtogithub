e26a2f74029a2eb2f846d97065f0f98a73366005
Arunkumar <k.arunkumarmca@yahoo.com>
2022-06-12 20:08:35 +0530

    configured Successful oracle connection

--- trainingproject/src/trainingproject/SampleToGitHub.java
+++ trainingproject/src/trainingproject/SampleToGitHub.java
@@ -1,13 +1,38 @@
 package trainingproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SampleToGitHub {
+	
+	public static void getDBResult() {
+		try {
+			
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:xe","sys as sysdba","ora123");
+			PreparedStatement ps = con.prepareStatement("select * from employee where id=?");
+			ps.setString(1, "101");
+		
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				System.out.println(rs.getString("id"));
+				System.out.println(rs.getString("name"));
+			}
+		} catch (ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+		} catch(SQLException sql) {
+			sql.printStackTrace();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 		
-		int a = 3;
-		int b = 2;
-		int total = a * b;
-		System.out.println("Total Value is: "+total);
+		SampleToGitHub.getDBResult();
 	}
 
 }
